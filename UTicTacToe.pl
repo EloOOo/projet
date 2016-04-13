a456befe470e4ee8d56b07a2b7728dc2e9841a27
elodie <bernarelodie@gmail.com>
2016-04-13 15:30:02 +0200

    retentative de push du prolog bien

--- UTicTacToe.pl
+++ UTicTacToe.pl
@@ -3,6 +3,7 @@
 % l = libre
 %plateau([[d,[l,l,l,l,l,l,l,l,l]], [d,[l,l,l,l,l,l,l,l,l]],[d,[l,l,l,l,l,l,l,l,l]],[l,l,l,l,l,l,l,l,l]],[d,[l,l,l,l,l,l,l,l,l]],[d,[l,l,l,l,l,l,l,l,l]],[d,[l,l,l,l,l,l,l,l,l]],[d,[l,l,l,l,l,l,l,l,l]],[d,[l,l,l,l,l,l,l,l,l]]]).
 
+sousPlateau([l,l,l,l,l,l,l,l,l]).
 symbole(x).
 symbole(o).
 libre(l).
@@ -25,6 +26,28 @@ winPlateau([[_,_], [_,_],[S,_],[_,_],[_,_],[S,_],[_,_],[_,_],[S,_]]):-!.
 winPlateau([[S,_], [_,_],[_,_],[_,_],[S,_],[_,_],[_,_],[_,_],[S,_]]):-!.
 winPlateau([[_,_], [_,_],[S,_],[_,_],[S,_],[_,_],[S,_],[_,_],[_,_]]):-!.
 
+move([[l,B,C,D,E,F,G,H,I],_], S, J,[[S,B,C,D,E,F,G,H,I],Cout]):-
+        evalSousPlateau([S,B,C,D,E,F,G,H,I],S,J,Cout).
+move([[A,l,C,D,E,F,G,H,I],_], S, J,[[A,S,C,D,E,F,G,H,I],Cout]):-
+        evalSousPlateau([A,S,C,D,E,F,G,H,I],S,J,Cout).
+move([[A,B,l,D,E,F,G,H,I],_], S, J,[[A,B,S,D,E,F,G,H,I],Cout]):-
+        evalSousPlateau([A,B,S,D,E,F,G,H,I],S,J,Cout).
+move([[A,B,C,l,E,F,G,H,I],_], S, J,[[A,B,C,S,E,F,G,H,I],Cout]):-
+        evalSousPlateau([A,B,C,S,E,F,G,H,I],S,J,Cout).
+move([[A,B,C,D,l,F,G,H,I],_], S, J,[[A,B,C,D,S,F,G,H,I],Cout]):-
+        evalSousPlateau([A,B,C,D,S,F,G,H,I],S,J,Cout).
+move([[A,B,C,D,E,l,G,H,I],_], S, J,[[A,B,C,D,E,S,G,H,I],Cout]):-
+        evalSousPlateau([A,B,C,D,E,S,G,H,I],S,J,Cout).
+move([[A,B,C,D,E,F,l,H,I],_], S, J,[[A,B,C,D,E,F,S,H,I],Cout]):-
+        evalSousPlateau([A,B,C,D,E,F,S,H,I],S,J,Cout).
+move([[A,B,C,D,E,F,G,l,I],_], S, J,[[A,B,C,D,E,F,G,S,I],Cout]):-
+        evalSousPlateau([A,B,C,D,E,F,G,S,I],S,J,Cout).
+move([[A,B,C,D,E,F,G,H,l],_], S, J,[[A,B,C,D,E,F,G,H,S],Cout]):-
+        evalSousPlateau([A,B,C,D,E,F,G,H,S],S,J,Cout).
+
+adversaire(x,o).
+adversaire(o,x).
+
 win(S,Plateau):-
         symbole(S),
         winPlateau(Plateau).
@@ -38,9 +61,6 @@ spPlein([]).
 spPlein([H|T]):-
         symbole(H),
         spPlein(T). 
-
-algo(P) :-
-        write(P).
 
 inverse(L,S):-
         inverse(L,[],S).
@@ -62,19 +82,64 @@ comptLignePG(Sp,S,Cout) :- % compte ligne possiblement/probablement/potentiellem
         symbole(S),
         remplace(Sp,S,[],Li),
         inverse(Li,[],L),
-        findall(Cout, winSousPlateau(L,S), R),
-        length(R,Cout).
+        findall(Cout, winSousPlateau(L,S), R),length(R,Cout).
         
-evalSousPlateauMoi(Sp,SJ1,SJ2,Cout):-
-        symbole(SJ1),
-        symbole(SJ2),
-        comptLignePG(Sp,SJ1,Cout1),
+evalSousPlateau(Sp,SNous,J,Cout):-
+        symbole(SNous),
+        adversaire(SNous,SJ2),
+        comptLignePG(Sp,SNous,Cout1),
         comptLignePG(Sp,SJ2,Cout2),
-        Cout is Cout1 - Cout2.
+        formuleEval(Cout1,Cout2,J,Cout).
 
-evalSousPlateauLui(Sp,SJ1,SJ2,Cout):-
-        symbole(SJ1),
-        symbole(SJ2),
-        comptLignePG(Sp,SJ1,Cout1),
-        comptLignePG(Sp,SJ2,Cout2),
+formuleEval(Cout1,Cout2,0,Cout):-
+        Cout is Cout1 - Cout2.
+formuleEval(Cout1,Cout2,1,Cout):-
         Cout is Cout2 - Cout1.
+
+concatene([],L,L).
+concatene([X|L1],L2,R):-
+        concatene(L1,[X|L2],R).
+
+etageSuivant([],_,_,Acc,Acc).
+etageSuivant([Sp|P],S,J,Acc,LLR) :-
+        placer(Sp,S,J,R),
+        append(R,Acc,NAcc),
+        etageSuivant(P,S,J,NAcc,LLR).
+
+placer([Sp|Cout],S,J,R):- % possibilité nonmember plus tard 
+        findall([L,Sp|Cout],move(Sp,S,J,L),R).
+
+        
+etatFinal([[[Sp,Cout]|LSp]|_],S,[[Sp,Cout]|LSp]) :-
+        winSousPlateau(Sp,S).
+etatFinal([_|LL],S,R) :-
+        etatFinal(LL,S,R).
+
+largeur([],_, _,_) :-
+        !,fail.
+
+largeur(Sp,S,J,LR) :-
+        etageSuivant(Sp,S,J,[],LL2),
+        verifSPGagnant(LL2,S,J, LR).
+             
+verifSPGagnant(LL,S,_,LR):-
+         etatFinal(LL,S,LR).
+verifSPGagnant(LL,S,J, LR):-
+        J1 is 1-J,
+        adversaire(S,A),
+        largeur(LL,A,J1,LR).             
+        
+pl(S,L) :-
+        sousPlateau(Sp),
+        evalSousPlateau(Sp,S,0,Cout),
+        largeur([[[Sp,Cout]]],S,0, Los),
+        inverse(Los,L).
+
+ 
+algo(P,SNous) :-
+        write(SNous),
+        write(P).
+
+
+              
+                        
