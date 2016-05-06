81bce9b7de73d8eaef4023bbe58b76dfd0d6b423
bCrivelli <benoit.crivelli@gmail.com>
2016-05-06 14:00:57 +0200

    correction bug prolog

--- src/prolog/UTicTacToe.pl
+++ src/prolog/UTicTacToe.pl
@@ -20,15 +20,6 @@ winSousPlateau([_,_,S,_,_,S,_,_,S],S).
 winSousPlateau([S,_,_,_,S,_,_,_,S],S).
 winSousPlateau([_,_,S,_,S,_,S,_,_],S).
 
-%verifie si P gagnant
-winPlateau([[S,_], [S,_],[S,_],[_,_],[_,_],[_,_],[_,_],[_,_],[_,_]]):-!.
-winPlateau([[_,_], [_,_],[_,_],[S,_],[S,_],[S,_],[_,_],[_,_],[_,_]]):-!.
-winPlateau([[_,_], [_,_],[_,_],[_,_],[_,_],[_,_],[S,_],[S,_],[S,_]]):-!.
-winPlateau([[S,_], [_,_],[_,_],[S,_],[_,_],[_,_],[S,_],[_,_],[_,_]]):-!.
-winPlateau([[_,_], [S,_],[_,_],[_,_],[S,_],[_,_],[_,_],[S,_],[_,_]]):-!.
-winPlateau([[_,_], [_,_],[S,_],[_,_],[_,_],[S,_],[_,_],[_,_],[S,_]]):-!.
-winPlateau([[S,_], [_,_],[_,_],[_,_],[S,_],[_,_],[_,_],[_,_],[S,_]]):-!.
-winPlateau([[_,_], [_,_],[S,_],[_,_],[S,_],[_,_],[S,_],[_,_],[_,_]]):-!.
 
 movePos([l,B,C,D,E,F,G,H,I], S,[S,B,C,D,E,F,G,H,I],1).
 movePos([A,l,C,D,E,F,G,H,I], S,[A,S,C,D,E,F,G,H,I],2).
@@ -39,27 +30,11 @@ movePos([A,B,C,D,E,l,G,H,I], S,[A,B,C,D,E,S,G,H,I],6).
 movePos([A,B,C,D,E,F,l,H,I], S,[A,B,C,D,E,F,S,H,I],7).
 movePos([A,B,C,D,E,F,G,l,I], S,[A,B,C,D,E,F,G,S,I],8).
 movePos([A,B,C,D,E,F,G,H,l], S,[A,B,C,D,E,F,G,H,S],9).
+movePos([A,B,C,D,E,F,G,H,I], _,[A,B,C,D,E,F,G,H,I],_).
 
 %Changement de joueur
 adversaire(x,o).
 adversaire(o,x).
-
-%test si le plateau est gagne
-win(S,Plateau):-
-        symbole(S),
-        winPlateau(Plateau).
-
-
-aGagnant(P,x):-
-        win(x,P).
-aGagnant(P,o):-
-        win(o,P).
-
-
-spPlein([]).
-spPlein([H|T]):-
-        symbole(H),
-        spPlein(T). 
 
 %inverse une liste
 inverse(L,S):-
@@ -99,75 +74,52 @@ formuleEval(Cout1,Cout2,0,Cout):-
 formuleEval(Cout1,Cout2,1,Cout):-
         Cout is Cout2 - Cout1.
 
-concatene([],L,L).
-concatene([X|L1],L2,R):-
-        concatene(L1,[X|L2],R).
-
-%Test si dans un liste de coup l'un d'eux est gagnant    
+%Test si dans une liste Sp l'un d'eux est gagnant    
 etatFinal([[[Sp,Cout]|LSp]|_],S,[[Sp,Cout]|LSp]) :-
         winSousPlateau(Sp,S).
 etatFinal([_|LL],S,R) :-
         etatFinal(LL,S,R).
 
+
 % alphaBeta(6,[x,l,o,l,l,x,l,o,l],x,-10000,10000,Move,_).
 % alphaBeta(6,[l,l,o,x,l,x,l,o,l],x,-10000,10000,Move,Cout,PosS).
+
+%alphaBeta
 % l'adversaire gagne on s'arrete
-alphaBeta(_,Sp,S,_,_,0,_,-1000,posS):-
+alphaBeta(_,Sp,S,_,_,0,_,-1000):-
         adversaire(S,A),
         winSousPlateau(Sp,A).
-% lors de l'appel Alpha = Tres Grand nb et Beta tres petit (100 et -100)
-alphaBeta(Prof,Sp,S, Alpha,Beta,Move,LMove,Cout,PosS):- 
+% continue
+alphaBeta(Prof,Sp,S, Alpha,Beta,Move,LMove,Cout):- 
         Prof > 0,
-        bestMoves(Sp,S,LMove,PosS),
-        LMove = [_|_], !, % on vérifie si la liste contient 1 seul élément ou +
+        bestMoves(Sp,S,LMove,_),
+        LMove = [_|_], !, % verifie si liste contient un elem ou +
         Prof1 is Prof- 1,
         Alpha1 is -Beta,
         Beta1 is -Alpha,
-        meilleurChoix(LMove, Sp, S,Prof1, Alpha1, Beta1, 0, Move, Cout,PosS).
-alphaBeta(_, Sp, S, _, _, 0,_, Cout,posS):-
+        meilleurChoix(LMove, Sp, S,Prof1, Alpha1, Beta1, 0, Move, Cout).
+alphaBeta(_, Sp, S, _, _, 0,_, Cout):-
         value(Sp, S,Cout).
 
-%Verifier si un coup ne fais pas gagner l'adversaire au TicTacToe
-%identifier le SP ou l'adversaire va jouer -> OK
-%Faire jouer l'adversaire dans ce SP
-%Si coup adverse provoque victoire adverse -> nouveau coup de nous
-%Sinon on garde notre coup
-%testCoup(Plateau,PlateauSimple,6,[l,l,o,x,l,x,l,o,l],x,-10000,10000,Move,LMove,Cout,PosS).
-/*testCoup(Plateau,PlateauSimple,Prof,Sp,S, Alpha,Beta,Move,LMove,Cout,PosS,Nb):-
-       Nb > 0,
-       findall(Move, movePos(Sp,S,Move,PosS), LstMove),
-       meilleurChoix(LstMove, Sp, S, Prof, Alpha, Beta, Move0, Move1, Value1,_),
-       alphaBeta(Prof,Sp,S, Alpha,Beta,Move0,LMove,Cout,PosS),
-*/
-
-%testSPDispo([l,l,l,o,l,o,l,l,l],5,Res).
-%test si le SP ou on doit joué est déjé gagné
-testSPDispo(Sps,PosSP,0):-
-         nth1(PosSP, Sps, S),
-         S = l.
-          
-% 1 = SP Gagné         
-testSPDispo(Sps,PosSP,1):-
-         nth1(PosSP, Sps, S),
-         S \= l.
-        
-meilleurChoix([H|T], Sp, S, Prof, Alpha, Beta, Move0, Move1, Cout1,PosS):-
-        movePos(Sp, S, NewSp,PosS), !,
+                      
+meilleurChoix([H|T], Sp, S, Prof, Alpha, Beta, Move0, Move1, Cout1):-
+        %move(Sp, S, NewSp), !,
+        movePos(H, S, NewSp,_), !,
         adversaire(S,A),
-        alphaBeta(Prof, NewSp, A, Alpha, Beta, _,_, CoutMin,posS),
+        alphaBeta(Prof, NewSp, A, Alpha, Beta, _,_, CoutMin,_),
         Cout is -CoutMin,
-        cutoff(H, Cout, Prof, Alpha, Beta, T, Sp, S, Move0, Move1, Cout1).
-meilleurChoix([], _, _, _, Alpha, _, Move, Move, Alpha,_).
+        coupe(H, Cout, Prof, Alpha, Beta, T, Sp, S, Move0, Move1, Cout1).
+meilleurChoix([], _, _, _, Alpha, _, Move, Move, Alpha).
 
-cutoff(_, Cout, Prof, Alpha, Beta, LstMove, Sp, S, Move0, Move1, Value1):-
+coupe(_, Cout, Prof, Alpha, Beta, LstMove, Sp, S, Move0, Move1, Value1):-
         Cout =< Alpha, !,
-        meilleurChoix(LstMove, Sp, S, Prof, Alpha, Beta, Move0, Move1, Value1,_).
-cutoff(Move, Cout, Prof, _, Beta, LstMove, Sp, S, _, Move1, Value1):-
+        meilleurChoix(LstMove, Sp, S, Prof, Alpha, Beta, Move0, Move1, Value1).
+coupe(Move, Cout, Prof, _, Beta, LstMove, Sp, S, _, Move1, Value1):-
         Cout < Beta, !,
-        meilleurChoix(LstMove, Sp, S, Prof, Cout, Beta, Move, Move1, Value1,_).
-cutoff(Move, Cout, _, _, _, _, _, _, _, Move, Cout).
+        meilleurChoix(LstMove, Sp, S, Prof, Cout, Beta, Move, Move1, Value1).
+coupe(Move, Cout, _, _, _, _, _, _, _, Move, Cout).
 
-premiereDiff([_|T], [_|T], 0):- !.
+premiereDiff([_|T], [_|T], 1):- !.
 premiereDiff([H|T], [H|T2], Index):-
         premiereDiff(T, T2, Index1),
         !,
@@ -180,7 +132,7 @@ remplaceOcc([H|T], I, X, [H|R]):-
         remplaceOcc(T, NI, X, R), !.
 remplaceOcc(L, _, _, L).
 
-% Déplacement gagnant
+% D�placement gagnant
 bestMoves(Sp,S,[Move],PosS):-
         %move(Sp,S,Move),
         movePos(Sp,S,Move,PosS),
@@ -207,40 +159,35 @@ value(Sp, S,Cout):-
 %Si num = 0 -->jouer partout -->1er coup croix
 %PlatU ->plateau complet
 %Sps ->plateau simple
-%num ->num du SP ou joué
-%SpPlay ->num du SP ou prolog é joué
-%CasePlay ->num de la case joué dans le SP
+%num ->num du SP ou jou�
+%SpPlay ->num du SP ou prolog � jou�
+%CasePlay ->num de la case jou� dans le SP
 
 %testJava([[l,l,l,x,l,x,l,l,l],[l,l,l,l,l,l,l,l,l],[l,l,l,l,l,l,l,l,l],[l,l,l,l,l,l,l,l,l],[l,l,l,o,l,o,l,l,l],[l,l,l,l,l,l,l,l,l],[l,l,l,l,l,l,l,l,l],[l,l,l,l,l,l,l,l,l],[l,l,l,l,l,l,l,l,l]],[l,x,l,l,l,l,l,l,l],1,x,SpPlay,CasePlay,L).
 %testJava([[x,x,x,x,x,x,x,x],[l,l,l,l,l,l,l,l,l],[l,l,l,l,l,l,l,l,l],[l,l,l,l,l,l,l,l,l],[l,l,l,o,l,o,l,l,l],[l,l,l,l,l,l,l,l,l],[l,l,l,l,l,l,l,l,l],[l,l,l,l,l,l,l,l,l],[l,l,l,l,l,l,l,l,l]],[x,l,x,x,x,x,x,x,x],1,x,SpPlay,CasePlay,L).
 
-testEgalite(Sps,S) :-
-        adversaire(S,A),
-        compteSPG(Sps,S,NbSPG1),
-        compteSPG(Sps,A,NbSPG2),
-        Tot is NbSPG1 + NbSPG2,
-        Tot \= 9.
 
 testJava(PlatU,Sps,10,S,SpPlay,CasePlay,NbSPG) :-
-        testEgalite(Sps,S),
         trouveSPLibre(Sps,SpPlay),
         nth1(SpPlay, PlatU, Sp),
-        alphaBeta(6,Sp,S, -10000,10000,Move,_,_,CasePlay),
+        alphaBeta(6,Sp,S, -10000,10000,Move,_,_),
+        premiereDiff(Sp, Move, CasePlay),
         winSousPlateau(Move,S),
         movePos(Sps,S,Nps,SpPlay),
         compteSPG(Nps,S,NbSPG);
         trouveSPLibre(Sps,SpPlay),
         nth1(SpPlay, PlatU, Sp),
-        alphaBeta(6,Sp,S, -10000,10000,_,_,_,CasePlay),
+        alphaBeta(6,Sp,S, -10000,10000,Move,_,_),
+        premiereDiff(Sp, Move, CasePlay),
         compteSPG(Sps,S,NbSPG).
-%le SP donné est libre
+%le SP donn� est libre
 testJava(PlatU,Sps,Num,S,SpPlay,CasePlay,NbSPG) :-
-        testEgalite(Sps,S),
         testSPDispo(Sps,Num,Libre),
         Libre is 0,
         SpPlay is Num,
         nth1(Num, PlatU, Sp),
-        alphaBeta(6,Sp,S, -10000,10000,Move,_,_,CasePlay),
+        alphaBeta(6,Sp,S, -10000,10000,Move,_,_),
+        premiereDiff(Sp, Move, CasePlay),
         winSousPlateau(Move,S),
         movePos(Sps,S,Nps,SpPlay),
         compteSPG(Nps,S,NbSPG);
@@ -248,17 +195,18 @@ testJava(PlatU,Sps,Num,S,SpPlay,CasePlay,NbSPG) :-
         Libre is 0,
         SpPlay is Num,
         nth1(Num, PlatU, Sp),
-        alphaBeta(6,Sp,S, -10000,10000,_,_,_,CasePlay),
+        alphaBeta(6,Sp,S, -10000,10000,Move,_,_),
+        premiereDiff(Sp, Move, CasePlay),
         compteSPG(Sps,S,NbSPG).
 
-%le SP donné est gagné ou nulle -> il faut jouer dans un autre SP (libre)
+%le SP donn� est gagn� ou nulle -> il faut jouer dans un autre SP (libre)
 testJava(PlatU,Sps,Num,S,SpPlay,CasePlay,NbSPG) :-
-        testEgalite(Sps,S),
         testSPDispo(Sps,Num,Libre),
         Libre is 1,
         trouveSPLibre(Sps,SpPlay),
         nth1(SpPlay, PlatU, Sp),
-        alphaBeta(6,Sp,S, -10000,10000,Move,_,_,CasePlay),
+        alphaBeta(6,Sp,S, -10000,10000,Move,_,_),
+        premiereDiff(Sp, Move, CasePlay),
         winSousPlateau(Move,S),
         movePos(Sps,S,Nps,SpPlay),
         compteSPG(Nps,S,NbSPG);
@@ -266,7 +214,8 @@ testJava(PlatU,Sps,Num,S,SpPlay,CasePlay,NbSPG) :-
         Libre is 1,
         trouveSPLibre(Sps,SpPlay),
         nth1(SpPlay, PlatU, Sp),
-        alphaBeta(6,Sp,S, -10000,10000,_,_,_,CasePlay),
+        alphaBeta(6,Sp,S, -10000,10000,Move,_,_),
+        premiereDiff(Sp, Move, CasePlay),
         compteSPG(Sps,S,NbSPG).
         
 
@@ -287,3 +236,28 @@ compteSPG([Symbole|R],S,PosSp1):-
         PosSp1 is PosSp+1.
 compteSPG([_|R],S,PosSp):-
         compteSPG(R,S,PosSp).       
+                    
+%Verifier si un coup ne fais pas gagner l'adversaire au TicTacToe
+%identifier le SP ou l'adversaire va jouer -> OK
+%Faire jouer l'adversaire dans ce SP
+%Si coup adverse provoque victoire adverse -> nouveau coup de nous
+%Sinon on garde notre coup
+%testCoup(Plateau,PlateauSimple,6,[l,l,o,x,l,x,l,o,l],x,-10000,10000,Move,LMove,Cout,PosS).
+/*testCoup(Plateau,PlateauSimple,Prof,Sp,S, Alpha,Beta,Move,LMove,Cout,PosS,Nb):-
+       Nb > 0,
+       findall(Move, movePos(Sp,S,Move,PosS), LstMove),
+       meilleurChoix(LstMove, Sp, S, Prof, Alpha, Beta, Move0, Move1, Value1,_),
+       alphaBeta(Prof,Sp,S, Alpha,Beta,Move0,LMove,Cout,PosS),
+*/
+
+%testSPDispo([l,l,l,o,l,o,l,l,l],5,Res).
+%test si le SP ou on doit jou� est d�j� gagn�
+testSPDispo(Sps,PosSP,0):-
+         nth1(PosSP, Sps, S),
+         S = l.
+          
+% 1 = SP Gagn�         
+testSPDispo(Sps,PosSP,1):-
+         nth1(PosSP, Sps, S),
+         S \= l.
+       
